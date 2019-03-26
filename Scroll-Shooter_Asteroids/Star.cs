b2394f0a106ace883d0db5776098ece36dcae0fb
Vasubanhu <shangrila1721@gmail.com>
2019-03-27 02:34:29 +0500

    правки

--- Scroll-Shooter_Asteroids/Star.cs
+++ Scroll-Shooter_Asteroids/Star.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Drawing;
-
+﻿using System.Drawing;
 
 namespace Scroll_Shooter_Asteroids
 {
     class Star : BaseObject
     {
+        Image img = Image.FromFile("..\\..\\img\\clover.png");
+        /// <summary>
+        /// Наследуемый конструктор объекта Star
+        /// </summary>
+        /// <param name="pos">начальные координаты объекта</param>
+        /// <param name="dir">направление движения объекта</param>
+        /// <param name="size">размер объекта</param>
         public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
         }
@@ -14,17 +19,18 @@ namespace Scroll_Shooter_Asteroids
         /// </summary>
         public override void Draw()
         {
-            Game.Buffer.Graphics.DrawLine(Pens.Wheat, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
-            Game.Buffer.Graphics.DrawLine(Pens.Blue, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+            Game.Buffer.Graphics.DrawImage(img, Pos.X, Pos.Y, 16, 16);
+            //--------------------------------------------------------
+            //Game.Buffer.Graphics.DrawLine(Pens.Wheat, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            //Game.Buffer.Graphics.DrawLine(Pens.Blue, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
         }
         /// <summary>
-        /// Переопределенный метод для движения звезд
+        /// Переопределенный метод для логики движения объекта Star
         /// </summary>
         public override void Update()
         {
             Pos.X = Pos.X + Dir.X;
             if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
         }
-
     }
 }
