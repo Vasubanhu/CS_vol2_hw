31fff654b102becdc26a76775c79b13734efe7df
Vasubanhu <shangrila1721@gmail.com>
2019-04-01 15:34:36 +0500

    -//-

---
+++ Scroll-Shooter_Asteroids/CustomInterfaces.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Scroll_Shooter_Asteroids
+{
+    /// <summary>
+    /// Интерфейс для опредления столкновения объектов
+    /// </summary>
+    interface ICollision
+    {
+        bool Collision(ICollision obj);
+        Rectangle Rect { get; }
+    }
+}
